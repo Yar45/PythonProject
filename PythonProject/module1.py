969d3cb63156f3a35aa5c7cae49e17e9018b424c
Yar45 <hot.ho@mail.ru>
2024-05-17 21:04:15 +0300

    added comments and edit doxyfile

--- PythonProject/module1.py
+++ PythonProject/module1.py
@@ -1,5 +1,15 @@
 def sum(a,b):
+    """! Sum numbers.
+    @param a   Num 1
+    @param b   Num 2.
+    @return  a + b.
+    """
     return a+b
 
 def sub(a,b):
+    """! Sub numbers.
+    @param a   Num 1
+    @param b   Num 2.
+    @return  a - b.
+    """
     return a-b
